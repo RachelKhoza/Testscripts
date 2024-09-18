1275e8e1b00e8247155090a62e67d902b67773f0
RachelKhoza <malete.rachel@gmail.com>
2024-09-18 06:15:02 +0200

    script to backup ebs

---
+++ DLMEbs.sh
@@ -0,0 +1,18 @@
+aws dlm create-lifecycle-policy \
+    --execution-role-arn arn:aws:iam::123456789012:role/AWSDataLifecycleManagerDefaultRole \
+    --description "Daily EBS backup lifecycle policy at 01:00 UTC" \
+    --state ENABLED \
+    --policy-details '{
+        "ResourceTypes": ["VOLUME"],
+        "TargetTags": [{"Key": "Backup", "Value": "True"}],
+        "Schedules": [{
+            "Name": "DailyBackup",
+            "CreateRule": {
+                "Interval": 24,
+                "IntervalUnit": "HOURS",
+                "Times": ["01:00"]
+            },
+            "RetainRule": {"Count": 7},
+            "TagsToAdd": [{"Key": "Snapshot", "Value": "DailyBackup"}]
+        }]
+    }'
